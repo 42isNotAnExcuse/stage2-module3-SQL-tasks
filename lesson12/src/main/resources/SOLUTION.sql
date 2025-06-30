2ddfe4a5d5ce28e6ad4a6ec2d120eaaf638c476a
Igor Bugaiov <proxipixel.gmail.com>
2025-06-30 20:33:21 +0200

    st2_m3_SQL updated init version

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,1 +1,1 @@
-DELETE FROM Student WHERE `group` >= 4; DELETE FROM Student WHERE id IN (SELECT student_id FROM Mark GROUP BY student_id HAVING MIN(mark) < 4); DELETE FROM PaymentType WHERE name = 'Daily'; DELETE FROM Mark WHERE mark < 7;
+DELETE FROM Student WHERE groupnumber >= 4; DELETE FROM Student WHERE id IN (SELECT student_id FROM Mark GROUP BY student_id HAVING MIN(mark) < 4); DELETE FROM PaymentType WHERE name = 'Daily'; DELETE FROM Mark WHERE mark < 7;
